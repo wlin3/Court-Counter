e3aa03145483dfe518a7eef8a6aedc9b3c11bfee
William Lin <02william.lin@gulllakecs.org>
2021-03-05 12:47:34 -0500

    Finished Adding Styles and Margins

--- CourtCounter/app/src/main/java/com/example/courtcounter/MainActivity.java
+++ CourtCounter/app/src/main/java/com/example/courtcounter/MainActivity.java
@@ -16,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
 
 
     int scoreTeamA = 0;
+    int scoreTeamB = 0;
 
     /**
      * Increase the score for Team A by 1 point.
@@ -40,6 +41,42 @@ public class MainActivity extends AppCompatActivity {
     public void addThreeForTeamA(View v) {
         scoreTeamA = scoreTeamA + 3;
         displayForTeamA(scoreTeamA);
+    }
+
+    /**
+     * Displays the given score for Team A.
+     */
+
+    /**
+     * Increase the score for Team B by 1 point.
+     */
+    public void addOneForTeamB(View v) {
+        scoreTeamB = scoreTeamB +1;
+        displayForTeamB(scoreTeamB);
+    }
+
+    /**
+     * Increase the score for Team B by 2 points.
+     */
+    public void addTwoForTeamB(View v) {
+        scoreTeamB = scoreTeamB +2;
+        displayForTeamB(scoreTeamB);
+    }
+
+
+    /**
+     * Increase the score for Team B by 3 points.
+     */
+    public void addThreeForTeamB(View v) {
+        scoreTeamB = scoreTeamB + 3;
+        displayForTeamB(scoreTeamB);
+    }
+
+    public void resetScore(View v) {
+        scoreTeamA = 0;
+        scoreTeamB = 0;
+        displayForTeamA(scoreTeamA);
+        displayForTeamB(scoreTeamB);
     }
 
     /**
